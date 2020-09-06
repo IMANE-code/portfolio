455d0c9cfcde1986e61089cfa2a1a4e8f1f13cb7
IMANE-code <57986793+IMANE-code@users.noreply.github.com>
2020-09-06 17:54:27 +0100

    modif

--- js/js.js
+++ js/js.js
@@ -6,3 +6,43 @@ $(window).on("scroll", function () {
     $("nav").removeClass("black");
   }
 });
+
+//display Form
+
+function dspmenu() {
+  document.getElementById("menu").style.display = "none";
+}
+function dspform() {
+  document.getElementById("form1").style.display = "block";
+  dspmenu();
+}
+
+//display Section
+function sectionHEADER() {
+  dspform();
+  document.getElementById("section-HEADER").style.display = "block";
+}
+function sectionABOUT() {
+  dspform();
+  document.getElementById("section-ABOUT").style.display = "block";
+}
+function sectionSERVICES() {
+  dspform();
+  document.getElementById("section-SERVICES").style.display = "block";
+}
+function sectionVEDEO() {
+  dspform();
+  document.getElementById("section-VEDEO").style.display = "block";
+}
+function sectionPROJECT() {
+  dspform();
+  document.getElementById("section-PROJECT").style.display = "block";
+}
+function sectionCONTACT() {
+  dspform();
+  document.getElementById("section-CONTACT").style.display = "block";
+}
+function sectionLOGIN() {
+  dspform();
+  document.getElementById("section-LOGIN").style.display = "block";
+}
